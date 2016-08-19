f76f55eccf63111e7c352e82d50906e37483e906
马雪明 <309201678@qq.com>
2016-08-19 11:11:34 +0800

    基础组件

---
+++ Comment/src/main/java/net/luoteng/comment/service/CommentService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.luoteng.comment.service;
+
+/**
+ *
+ * @author suetming <suetming.ma at gmail.com>
+ * Copyright(c) @2016 Luoteng Company, Inc.  All Rights Reserved.
+ */
+public interface CommentService {
+
+}
