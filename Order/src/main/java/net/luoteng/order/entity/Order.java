e152702f499d2d3cb2ae0597e6ef8abc73ffe5dc
马雪明 <309201678@qq.com>
2016-08-19 11:55:31 +0800

    支付&订单

---
+++ Order/src/main/java/net/luoteng/order/entity/Order.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.luoteng.order.entity;
+
+import net.luoteng.entity.AbstractTimeScopeEntity;
+
+/**
+ * 订单 
+ *
+ * @author suetming <suetming.ma at gmail.com>
+ * Copyright(c) @2016 Luoteng Company, Inc.  All Rights Reserved.
+ */
+public class Order extends AbstractTimeScopeEntity {
+
+}
