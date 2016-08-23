53d8fc1271ccd67a392d934f60e42de7890b9bc5
马雪明 <309201678@qq.com>
2016-08-23 09:04:42 +0800

    接口

--- Fund/src/main/java/net/luoteng/fund/dao/FundRecordDAO.java
+++ Fund/src/main/java/net/luoteng/fund/dao/FundRecordDAO.java
@@ -6,9 +6,15 @@
 
 package net.luoteng.fund.dao;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import javax.transaction.Transactional;
 import net.luoteng.fund.entity.FundRecord;
+import net.luoteng.fund.enums.FundRecordOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * 
@@ -19,6 +25,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 @Transactional
 public interface FundRecordDAO extends PagingAndSortingRepository<FundRecord, String> {
 
-    
+    @Query("select fr from FundRecord fr where fr.userId=:userId and fr.operation in :operations")
+    public Page<FundRecord> listByUser(@Param("userId") String userId, @Param("operations") List<FundRecordOperation> operations, Pageable pageable);
     
 }
--- Fund/src/main/java/net/luoteng/fund/service/impl/FundRecordServiceImpl.java
+++ Fund/src/main/java/net/luoteng/fund/service/impl/FundRecordServiceImpl.java
@@ -8,9 +8,11 @@ package net.luoteng.fund.service.impl;
 
 import java.awt.print.Pageable;
 import java.util.List;
+import net.luoteng.fund.dao.FundRecordDAO;
 import net.luoteng.fund.entity.FundRecord;
 import net.luoteng.fund.enums.FundRecordOperation;
 import net.luoteng.fund.service.FundRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
@@ -23,13 +25,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class FundRecordServiceImpl implements FundRecordService {
 
+    @Autowired
+    FundRecordDAO recordDAO;
+    
     @Override
     public Page<FundRecord> listByUser(String userId, List<FundRecordOperation> operations, Pageable pageable) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return recordDAO.listByUser(userId, operations, pageable);
     }
 
     @Override
-    public FundRecord save(FundRecord t) {
+    public FundRecord save(FundRecord model) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
