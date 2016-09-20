48de76843533b12e22362b6c16f7789f798b9b6d
马雪明 <309201678@qq.com>
2016-09-20 15:43:20 +0800

    sina

---
+++ Sina/src/main/java/net/luoteng/sina/service/SinaService.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2016 suetming <suetming.ma at gmail.com>.
+ *
+ * This work is licensed under the 
+ * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit 
+ *
+ *      http://creativecommons.org/licenses/by-nc-nd/3.0/
+ *
+ * or send a letter to Creative Commons, 444 Castro Street, Suite 900, 
+ * Mountain View, California, 94041, USA.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.luoteng.sina.service;
+
+/**
+ * 
+ *
+ * @author suetming <suetming.ma at gmail.com>
+ * Copyright(c) @2016 Luoteng Company, Inc.  All Rights Reserved.
+ */
+public interface SinaService {
+
+}
