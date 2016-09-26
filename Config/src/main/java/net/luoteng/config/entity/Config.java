5ba654015ee5e141ddc08d7f3c367b5a2c865e47
马雪明 <309201678@qq.com>
2016-09-26 13:23:33 +0800

    configuration

---
+++ Config/src/main/java/net/luoteng/config/entity/Config.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016 suetming <suetming.ma at gmail.com>.
+ *
+ * This work is licensed under the 
+ * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit 
+ *
+ *      http://creativecommons.org/licenses/by-nc-nd/3.0/
+ *
+ * or send a letter to Creative Commons, 444 Castro Street, Suite 900, 
+ * Mountain View, California, 94041, USA.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.luoteng.config.entity;
+
+import javax.persistence.Lob;
+import net.luoteng.entity.AbstractTimeScopeEntity;
+
+/**
+ * dynamic configuration
+ *
+ * @author suetming <suetming.ma at gmail.com>
+ * Copyright(c) @2016 Luoteng Company, Inc.  All Rights Reserved.
+ */
+public class Config extends AbstractTimeScopeEntity {
+
+    /**
+     * 类名
+     */
+    private String classSimpleName;
+    
+    /**
+     * 包名
+     */
+    private String packageName;
+    
+    /**
+     * 私有数据
+     */
+    @Lob
+    private String priv;
+    
+    
+}
